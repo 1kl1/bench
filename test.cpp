043bf8fa5cdd3bf515afe3a7aa2d033509148781
1kl1 <mamy0320@naver.com>
2021-07-18 23:25:58 +0900

    #1157

--- test.cpp
+++ test.cpp
@@ -1,27 +1,42 @@
-#include <stdio.h>
+// #include <stdio.h>
 #include <iostream>
 #include <string>
+// #include <vector>
 
 using namespace std;
 
 int main()
 {
-    int N;
-    cin >> N;
+    string str;
+    cin >> str;
 
-    for (int i = 0; i < N; i++)
+    int arr[32] = {
+        0,
+    };
+    int max = -1;
+    char maxChar;
+    for (int i = 0; i < str.length(); i++)
     {
-        int s;
-        cin >> s;
-        string str;
-        cin >> str;
-        for (int j = 0; j < str.length(); j++)
-            for (int k = 0; k < s; k++)
-                cout << str.at(j);
-        cout << endl;
+        int seq = (int)str[i] - 65;
+        if (seq >= 32)
+            seq -= 32;
+
+        arr[seq]++;
+        if (max < arr[seq])
+        {
+            maxChar = (char)(seq + 65);
+            max = arr[seq];
+        }
+        else if (max == arr[seq])
+        {
+            maxChar = '?';
+        }
     }
+    cout << maxChar << endl;
 
     return 0;
 }
 
+//cin >> var
 //cin.ignore(32767, '\n');
+// 65-A, 97-a
