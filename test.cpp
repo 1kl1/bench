fd941203a47feedd225ae7aebe58d96b9dfcc90c
1kl1 <mamy0320@naver.com>
2021-07-19 23:21:51 +0900

    #1712

--- test.cpp
+++ test.cpp
@@ -7,36 +7,18 @@ using namespace std;
 
 int main()
 {
-    int N;
-    cin >> N;
-    int tot = 0;
-    for (int i = 0; i < N; i++)
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (b >= c)
     {
-        string word;
-        cin >> word;
-        bool isGroup = true;
-        char befChar = word.at(0);
-        bool arr[26] = {
-            false,
-        };
-        arr[befChar - 97] = true;
-        for (int j = 1; j < word.length(); j++)
-        {
-            if (befChar != word[j])
-            {
-                if (arr[word[j] - 97])
-                {
-                    isGroup = false;
-                }
-                arr[befChar - 97] = true;
-            }
-            befChar = word.at(j);
-        }
-        if (isGroup)
-            tot++;
+        cout << "-1" << endl;
+        return 0;
     }
-
-    cout << tot << endl;
+    else
+    {
+        int bep = (int)(a / (c - b));
+        cout << (bep + 1) << endl;
+    }
 
     return 0;
 }
