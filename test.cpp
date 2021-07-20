002bbfb89ea431b0e8651a8be3ea1f8add8a8bc4
1kl1 <mamy0320@naver.com>
2021-07-20 22:44:45 +0900

    #1011

--- test.cpp
+++ test.cpp
@@ -1,23 +1,54 @@
 // #include <stdio.h>
 #include <iostream>
-#include <string>
+// #include <string>
 // #include <vector>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    int a, b, c;
-    cin >> a >> b >> c;
-    if (b >= c)
+    int N;
+    cin >> N;
+
+    for (int idx = 0; idx < N; idx++)
     {
-        cout << "-1" << endl;
-        return 0;
-    }
-    else
-    {
-        int bep = (int)(a / (c - b));
-        cout << (bep + 1) << endl;
+        int x, y;
+        cin >> x >> y;
+        int dist = y - x;
+        if (dist <= 4)
+        {
+            switch (dist)
+            {
+            case 1:
+                cout << 1 << endl;
+                break;
+            case 2:
+                cout << 2 << endl;
+                break;
+            case 3:
+                cout << 3 << endl;
+                break;
+            case 4:
+                cout << 3 << endl;
+                break;
+            }
+            continue;
+        }
+        int n = (int)sqrt(dist);
+        // cout << n << endl;
+        if (n * n == dist)
+        {
+            cout << n * 2 - 1 << endl;
+        }
+        else if (n * n + n < dist)
+        {
+            cout << n + n + 1 << endl;
+        }
+        else
+        {
+            cout << n + n << endl;
+        }
     }
 
     return 0;
