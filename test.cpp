b3d222a8f494fc9e20ee575fc6e906f110b5f13f
1kl1 <mamy0320@naver.com>
2021-07-22 23:20:20 +0900

    #2292

--- test.cpp
+++ test.cpp
@@ -1,40 +1,24 @@
 #include <stdio.h>
 #include <iostream>
 // #include <string>
-#include <vector>
-#include <cmath>
+// #include <vector>
+// #include <cmath>
 
 using namespace std;
-
-void che(int m, int n)
-{
-    bool arr[n + 1];
-    for (int i = 0; i <= n; i++)
-        arr[i] = false;
-
-    arr[0] = arr[1] = true;
-    int sqr = sqrt(n);
-    for (int i = 2; i <= sqr; i++)
-    {
-        if (arr[i])
-            continue;
-        for (int j = i * 2; j <= n; j += i)
-            arr[j] = true;
-    }
-    for (int i = m; i <= n; i++)
-    {
-        if (arr[i] == false)
-            printf("%d\n", i);
-    }
-}
 
 int main()
 {
 
-    int M, N;
-    cin >> M >> N;
-
-    che(M, N);
+    int n;
+    cin >> n;
+    int c = 1;
+    n -= c;
+    while (n > 0)
+    {
+        n -= c * 6;
+        c++;
+    }
+    cout << c << endl;
 
     return 0;
 }
