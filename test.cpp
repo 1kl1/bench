708f68e2bef6c9089dea41e2ddff69627868c353
1kl1 <mamy0320@naver.com>
2021-07-24 13:48:18 +0900

    #2775

--- test.cpp
+++ test.cpp
@@ -6,18 +6,29 @@
 
 using namespace std;
 
+int floor(int x, int y)
+{
+    if (x == 1)
+    {
+        return 1;
+    }
+    if (y == 0)
+    {
+        return x;
+    }
+    return floor(x - 1, y) + floor(x, y - 1);
+}
+
 int main()
 {
     int T;
     cin >> T;
     for (int i = 0; i < T; i++)
     {
-        int H, W, N;
-        cin >> H >> W >> N;
-
-        int ho = ceil(N / (H * 1.0));
-        int lvl = N % H == 0 ? H : N % H;
-        printf("%d%02d\n", lvl, ho);
+        int k, n;
+        cin >> k;
+        cin >> n;
+        printf("%d\n", floor(n, k));
     }
 
     return 0;
