c9191cbb7ec5a1aa58f081659689c7bcd7f813fc
1kl1 <mamy0320@naver.com>
2021-07-25 22:42:56 +0900

    #1002

--- test.cpp
+++ test.cpp
@@ -1,40 +1,59 @@
 #include <stdio.h>
 #include <iostream>
-// #include <string>
+#include <string>
 // #include <vector>
 #include <cmath>
 
 using namespace std;
 
-int floor(int x, int y)
-{
-    if (x == 1)
-    {
-        return 1;
-    }
-    if (y == 0)
-    {
-        return x;
-    }
-    return floor(x - 1, y) + floor(x, y - 1);
-}
-
 int main()
 {
     int T;
     cin >> T;
-    for (int i = 0; i < T; i++)
-    {
-        int k, n;
-        cin >> k;
-        cin >> n;
-        printf("%d\n", floor(n, k));
-    }
 
+    for (int t = 0; t < T; t++)
+    {
+        int x1, y1, r1, x2, y2, r2;
+        scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+        if (x1 == x2 && y1 == y2)
+        {
+            if (r1 == r2)
+                printf("-1\n");
+            else
+                printf("0\n");
+
+            continue;
+        }
+
+        double dist = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+        int k = r1 + r2;
+        if (dist > k)
+            cout << "0";
+        else if (dist == k)
+            cout << "1";
+        else
+        {
+            if (r1 > dist || r2 > dist)
+            {
+                int bigOne = r1 > r2 ? r1 : r2;
+                int smallOne = r1 > r2 ? r2 : r1;
+                if (bigOne == smallOne + dist)
+                    cout << "1";
+                else if (bigOne > smallOne + dist)
+                    cout << "0";
+                else
+                    cout << "2";
+            }
+            else
+            {
+                cout << "2";
+            }
+        }
+
+        cout << endl;
+    }
     return 0;
 }
 
-//cin >> var
 //cin.ignore(32767, '\n');
 // 65-A, 97-a
-// double sqrt()
