58f3a028477b9ddbabaeec325697d2f76710cb7e
1kl1 <mamy0320@naver.com>
2021-07-31 22:43:23 +0900

    #1018

--- code.cpp
+++ code.cpp
@@ -6,35 +6,58 @@
 
 using namespace std;
 
+char _swap(char flag)
+{
+    if (flag == 'W')
+        flag = 'B';
+    else
+        flag = 'W';
+    return flag;
+}
+
 int main()
 {
-    int N;
+    int N, M;
+    cin >> N >> M;
+    vector<string> map;
 
-    cin >> N;
-
-    vector<int> weight;
-    vector<int> height;
     for (int i = 0; i < N; i++)
     {
-        int w, h;
-        cin >> w >> h;
-        weight.push_back(w);
-        height.push_back(h);
+        string input;
+        cin >> input;
+        map.push_back(input);
     }
+    int _min = 999999;
 
-    for (int i = 0; i < N; i++)
+    for (int y = 7; y < N; y++)
     {
-        int cnt = 1;
-        for (int j = 0; j < N; j++)
+        for (int x = 7; x < M; x++)
         {
-            if (weight[i] < weight[j] && height[i] < height[j])
-                cnt++;
+            char flag = 'B';
+            int cnt = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (map[y - 7 + i][x - 7 + j] != flag)
+                    {
+                        cnt++;
+                    }
+                    flag = _swap(flag);
+                }
+                flag = _swap(flag);
+            }
+            if (_min > cnt)
+                _min = cnt;
+            if (_min > 64 - cnt)
+                _min = 64 - cnt;
         }
-        cout << cnt;
-        if (i != N - 1)
-            cout << " ";
     }
-    cout << endl;
+
+    if (_min == 999999)
+        _min = 0;
+    cout << _min << endl;
+
     return 0;
 }
 
