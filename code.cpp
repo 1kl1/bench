15a33bfdb147961b7de46d40741838fb74f3981b
1kl1 <mamy0320@naver.com>
2021-07-31 14:21:38 +0900

    #2447

--- code.cpp
+++ code.cpp
@@ -2,24 +2,54 @@
 #include <iostream>
 // #include <string>
 // #include <vector>
-#include <cmath>
+// #include <cmath>
 
 using namespace std;
 
-int fibo(int n)
+int N;
+char map[2188][2188];
+
+void star(int n, int x, int y, bool flag)
 {
     if (n == 1)
-        return 1;
-    else if (n == 0)
-        return 0;
-    return fibo(n - 1) + fibo(n - 2);
+    {
+        if (flag)
+            map[x][y] = '*';
+        else
+            map[x][y] = ' ';
+    }
+    else
+    {
+        x *= 3;
+        y *= 3;
+        star(n / 3, x, y, flag && true);
+        star(n / 3, x + 1, y, flag && true);
+        star(n / 3, x + 2, y, flag && true);
+
+        star(n / 3, x, y + 1, flag && true);
+        star(n / 3, x + 1, y + 1, false);
+        star(n / 3, x + 2, y + 1, flag && true);
+
+        star(n / 3, x, y + 2, flag && true);
+        star(n / 3, x + 1, y + 2, flag && true);
+        star(n / 3, x + 2, y + 2, flag && true);
+    }
+    return;
 }
 
 int main()
 {
-    int N;
     cin >> N;
-    cout << fibo(N) << endl;
+    star(N, 0, 0, true);
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%c", map[i][j]);
+        }
+        cout << endl;
+    }
     return 0;
 }
 
